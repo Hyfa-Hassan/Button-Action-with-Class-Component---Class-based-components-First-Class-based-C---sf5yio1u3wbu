5f2401c3a4989105abff0cabb4b99b3270c99452
Hyfa-Hassan <110564591+Hyfa-Hassan@users.noreply.github.com>
2022-11-01 22:11:15 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,21 @@ import '../styles/App.css';
 class App extends Component {
     constructor(props) {
 		super(props);
+	    	this.state = {
+			show:false
+		}
 	};
+	handleClick(){
+	    	this.setState(true)
+	}
 
     render() {
     	return(
     		<div id="main">
-			<button id="click">Click</button>
+			<button id="click" type="submit" onClick={()=>{this.handleClick()}}>Click</button>
+			{this.state.show === true ? (
+        			<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+     			) : undefined }
     		</div>
     	);
     }
@@ -18,3 +27,21 @@ class App extends Component {
 
 export default App;
 
+
+
+// function App() {
+//   const [show, setShow] = useState(false)
+//   const handleClick = () => {
+//         // console.log('djgfdj')
+//         setShow(true)
+//   }
+//   return (
+//     <div id="main">
+//        <button id="click" type="submit" onClick={()=>{handleClick()}}>Click</button>
+//        {show === true ? (
+//         <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy
+//         </p>
+//       ) : undefined }
+//     </div>
+//   );
+// }
